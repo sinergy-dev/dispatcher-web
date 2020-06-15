4122ae627f12a1079a2d3d5546cf1fb4b822e222
rama11 <prof.agastyo@gmail.com>
2020-06-15 14:47:39 +0700

    add refrance for firebase

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Carbon\Carbon;
 
 // use Kreait\Firebase;
 // use Kreait\Firebase\Factory;
@@ -31,9 +32,29 @@ class JobController extends Controller
     }
 
     public function testLiveNotification(){
-    	$reference = $this->database->getReference('notification');
-    	$snapshot = $reference->getSnapshot();
+    	$reference = $this->database->getReference('notification/web-notification');
+    	$snapshot = $reference->orderByKey()->getSnapshot();
     	$snapshot->getValue();
+
+    	$this->database->getReference('notification/web-notification/' . strval(sizeof($snapshot->getValue())) )
+			->set([
+				'to' => 'moderator@sinergy.co.id',
+				'from' => 'agastya@sinergy.co.id',
+				'title' => 'Job Update',
+				'message' => 'Job Has been update from day 1',
+				'status' => 'unread',
+				'date_time' => Carbon::now()->timestamp,
+				'showed' => false,
+			]);
+
+    	// to: data.to,
+     //    from: data.from,
+     //    title: data.title,
+     //    message: data.message,
+     //    status: "read",
+     //    date_time : data.date_time,
+     //    showed : true
+
 
     	// return $reference->getSnapshot();
     	// $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/eod-dev-key.json');
@@ -45,8 +66,11 @@ class JobController extends Controller
 		// 	->createDatabase();
 
 		echo "<pre>";
-		// print_r( $reference->getSnapshot());
-		print_r( $snapshot->getValue('test'));
+		// print_r( $reference->getValue());
+		print_r( strval(sizeof($snapshot->getValue())));
+		print_r( gettype(""));
+
+		// print_r( sizeof($snapshot->getValue()) - 1);
 		echo "</pre>";
 
 		// return $firebase;
