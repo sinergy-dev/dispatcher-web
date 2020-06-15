d4c6a932579ab89704c7598b6053e7f3a0371000
rama11 <prof.agastyo@gmail.com>
2020-06-15 09:35:22 +0700

    add more property and some atribute

--- app/User.php
+++ app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use DB;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -16,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','id_type'
     ];
 
     /**
@@ -36,4 +38,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getUserTypeAttribute(){
+        return DB::table('users_type')->select('name')
+            ->where('id','=',$this->id_type)
+            ->first()
+            ->name;
+    }
 }
