3dc5c43970316df6815b090866609baa05a9e7b8
rama11 <prof.agastyo@gmail.com>
2020-06-16 21:54:40 +0700

    add letter of assignment template

---
+++ resources/views/pdf/letter_of_assignment.blade.php
@@ -0,0 +1,154 @@
+<h1 style="text-align: center;">Letter of Assignment</h1>
+<h3 style="text-align: center;"><u>No : LoA/001/VI/2020</u></h3>
+
+<p>This letter we issue addressed to the customer to allow work to be carried out properly by personnel who have met the requirements and we appoint below</p>
+<hr>
+<p>Person = an engineer who is dispatched to the location and assigned to do the work</p>
+<table style="width: 100%">
+	<tr>
+		<td style="width: 20%;text-align: center;">
+			<img style="width: 150px;height: 200px;" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+		</td>
+		<td style="width: 40%">
+			<table>
+				<tr>
+					<td><b>Surname</b></td>
+					<td> : </td>
+					<td>Rama</td>
+				</tr>
+				<tr>
+					<td><b>NIK</b></td>
+					<td> : </td>
+					<td>876123875419812</td>
+				</tr>
+				<tr>
+					<td><b>Address</b></td>
+					<td> : </td>
+					<td>60 S. Acacia St. Aberdeen, SD 57401</td>
+				</tr>
+			</table>
+		</td>
+		<td style="width: 40%">
+			<table>
+				<tr>
+					<td><b>Given Name</b></td>
+					<td> : </td>
+					<td>Agastya</td>
+				</tr>
+				<tr>
+					<td><b>Place, Date of Birth</b></td>
+					<td> : </td>
+					<td>Malang, 3 October 1999</td>
+				</tr>
+				<tr>
+					<td><b>Phone Number</b></td>
+					<td> : </td>
+					<td>847-597-3893</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+<hr>
+<p>With the work we can describe as follows : </p>
+<table>
+	<tr>
+		<td><b>Job Title</b></td>
+		<td>:</td>
+		<td>Upgrade PC KC Madura</td>
+	</tr>
+	<tr>
+		<td><b>Job Category</b></td>
+		<td>:</td>
+		<td>Hardware Engineer</td>
+	</tr>
+	<tr>
+		<td><b>Job Location</b></td>
+		<td>:</td>
+		<td>Madura - Jawa Timur - Jawa</td>
+	</tr>
+	<tr>
+		<td><b>Job Address</b></td>
+		<td>:</td>
+		<td>Jl. Raya Klp. Dua No.9, RT.1/RW.5, Klp. Dua, Kec. Kb. Jeruk</td>
+	</tr>
+</table>
+<table>
+	<tr>
+		<td><b>Job Desciption</b></td>
+		<td><b>Job Requirment</b></td>
+	</tr>
+	<tr>
+		<td style="vertical-align: top;">
+			<ul>
+				<li>Developing a highly concurent and distrubed system</li>
+				<li>Performance Optimazitation and problem diagnosis</li>
+				<li>Designing/Developint for high-availability</li>
+				<li>Designing/Developing and testing new features</li>
+				<li>Estimating the effort requeired to develop and implement</li>
+				<li>Help define coding standrads and develpo process</li>
+				<li>Willing to learn & adapt different technologies</li>
+				<li>Create internal software</li>
+			</ul>
+		</td>
+		<td style="vertical-align: top;">
+			<ul>
+				<li>Experience with HTTP and RPC base service</li>
+				<li>Avibility to carefully breakdown the problem</li>
+				<li>Be abel to debug non-trivial application code</li>
+				<li>Experience with SQL and NoSQL database</li>
+				<li>Proficient with Git, and Github</li>
+				<li>Strong Data Structures & Algorithms concepts</li>
+			</ul>
+		</td>
+	</tr>
+</table>
+<table style="width: 100%" >
+	<tr >
+		<td style="width: 50%"><b>Customer</b></td>
+		<td style="width: 50%"><b>PIC (Person In Charge)</b></td>
+	</tr>
+	<tr>
+		<td style="padding-left: 10px">
+			<b>BPJS Kesehatan</b>
+			<br>Jl. Raya Klp. Dua No.9, RT.1/RW.5, Klp. Dua, Kec. Kb. Jeruk, Kota Jakarta Barat,
+		</td>
+		<td style="padding-left: 10px">
+			<b>Briliyan Aditya</b>
+			<br>082346123874
+			<br>briliyan@bpjskesehatan.go.id
+		</td>
+	</tr>
+</table>
+<hr>
+<p>Furthermore, by signing this letter, we hope that the work can start immediately so that it can produce satisfying results.</p>
+<br>
+<table style="width: 100%" >
+	<tr>
+		<td style="width: 50%">
+			<div style="text-align: center">
+				<img style="width: 100px" src="https://raw.githubusercontent.com/SimpleSoftwareIO/simple-qrcode/master/docs/imgs/example-1.png?raw=true">
+				<br><i>for detail information</i>
+			</div>
+		</td>
+		<td style="text-align:center; width: 50%">
+			Moderator Dispatcher
+			<br>
+			<br>
+			<br>
+			<br>
+			<b><u>Budi Karuniawan</u></b><br>
+			<b>PT. Sinergy Informasi Pratama</b>
+		</td>
+	</tr>
+	<!-- <tr>
+		<td style="text-align:center; width: 100%" colspan="2">
+			Person In Charge
+			<br>
+			<br>
+			<br>
+			<br>
+			<b><u>Briliyan Aditya</u></b>
+		</td>
+	</tr> -->
+</table>
